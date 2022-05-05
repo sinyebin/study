aed4f723d086f084ee2f48ef052ce830b00651ef
bin <dpqls0625@naver.com>
2022-05-05 16:45:10 +0900

    1로 만들기

--- 백준/back1463.java
+++ 백준/back1463.java
@@ -1,27 +1,15 @@
 package 백준;
 import java.util.*;
 public class back1463 {
-    static Integer[]dp;
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         int x=scanner.nextInt();
-        dp=new Integer[x+1];
-        dp[0]=0;
-        dp[1]=0;
-        System.out.println(make(x));
+        System.out.println(make(x,0));
     }
-    static int make(int x){
-        if(dp[x]==null){
-            if(x%6==0){
-                dp[x]=Math.min(make(x-1)+1, Math.min(make(x/2),make(x/3)));
-            }else if(x%3==0){
-                dp[x]=Math.min(make(x-1)+1, make(x/3));
-            }else if(x%2==0){
-                dp[x]=Math.min(make(x-1)+1, make(x/2));
-            }else{
-                dp[x]=make(x-1)+1;
-            }
+    static int make(int x, int count){
+        if(x<2){
+            return count;
         }
-        return dp[x];
+        return Math.min(make(x/2,count+1+(x%2)),make(x/3,count+1+(x%3)));
     }
 }
