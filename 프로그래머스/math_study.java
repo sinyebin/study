d035458aa5e97dd8c496b8f086dff475ea6405e6
bin <dpqls0625@naver.com>
2022-05-07 00:15:19 +0900

    수학 공부

---
+++ 프로그래머스/math_study.java
@@ -0,0 +1,27 @@
+package 프로그래머스;
+
+public class math_study {
+    public static void main(String args[]){
+        int n=15;
+        solution(n);
+    }
+    static int solution(int n) {
+        int []arr=new int[n/2+1];
+        int answer = 1;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                if(arr[i]>=n){
+                    break;
+                }
+                arr[i]+=j+1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==n){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
