62a0977d72caefd7ba69734355720be2a9ab2031
bin <dpqls0625@naver.com>
2022-05-07 15:05:43 +0900

    .

---
+++ 프로그래머스/k_intern3.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+import java.util.*;
+public class k_intern3 {
+    public static void main(String args[]){
+        int alp=10;
+        int cop=10;
+        int [][]problems={{10,15,2,1,2},{20,20,3,3,4}};
+        solution(alp, cop, problems);
+    }
+    static int solution(int alp, int cop, int[][] problems) {
+        int answer = 0;
+        for(int i=0;i<problems.length;i++){
+            int n_alp=0;
+            int n_cop=0;
+            
+        }
+        return answer;
+    }
+}
