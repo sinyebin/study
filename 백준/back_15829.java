eb245ad2e56d68a9e6f09d95b3ed51e71645454b
bin <dpqls0625@naver.com>
2022-05-09 21:32:26 +0900

    Hashing

--- 백준/back_15829.java
+++ 백준/back_15829.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class back_15829 {
     public static void main(String args[]){
         Scanner scanner=new Scanner(System.in);
-        int answer=0;
+        long answer=0;
+        long r=1;
+        int p=1234567891;
         int num=scanner.nextInt();
         scanner.nextLine();
         String str=scanner.nextLine();
         for(int i=0;i<num;i++){
             int a = str.charAt(i)-96;
-            answer+= (a*Math.pow(31, i)%1234567891);
+            answer+= (a*r)%p;
+            r=(r*31)%p;
         }
-        System.out.println(answer);
+        System.out.println(answer%p);
     }
 }
