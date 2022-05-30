e5b7870c465f2f639e17c7a5fbbf906a95f58717
bin <dpqls0625@naver.com>
2022-05-30 18:53:34 +0900

    몫과 나머지 출력하기

---
+++ swea/swea2029.java
@@ -0,0 +1,22 @@
+package swea;
+import java.util.Scanner;
+public class swea2029 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int num1=sc.nextInt();
+            int num2=sc.nextInt();
+            System.out.println("#"+test_case+" "+num1/num2+" "+num1%num2);
+			/////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+				 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			 */
+			/////////////////////////////////////////////////////////////////////////////////////////////
+
+		}
+    }
+}
