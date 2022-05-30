c852734e0820aef7e3e4f6cb2649d68b39442e65
bin <dpqls0625@naver.com>
2022-05-30 18:01:44 +0900

    신문 헤드라인

---
+++ swea/swea2047.java
@@ -0,0 +1,11 @@
+package swea;
+import java.util.Scanner;
+public class swea2047 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		String str=sc.nextLine();
+        str=str.toUpperCase();
+        System.out.println(str);
+    }
+}
