220f382844d95c3141ec286c6105a980bb2d659d
bin <dpqls0625@naver.com>
2022-05-30 18:46:24 +0900

    스탬프 찍기

---
+++ swea/swea2046.java
@@ -0,0 +1,14 @@
+package swea;
+import java.util.Scanner;
+public class swea2046 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+		{
+			System.out.print("#");
+        }
+    }
+}
