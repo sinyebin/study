f06ea3f20c95f7f391e773ffec3c529a6b4168ec
bin <dpqls0625@naver.com>
2022-05-30 18:57:34 +0900

    대각선 출력하기

---
+++ swea/swea2027.java
@@ -0,0 +1,12 @@
+package swea;
+import java.util.Scanner;
+public class swea2027 {
+    public static void main(String args[]) throws Exception
+	{
+        System.out.println("#++++");
+        System.out.println("+#+++");
+        System.out.println("++#++");
+        System.out.println("+++#+");
+        System.out.println("++++#");
+    }
+}
