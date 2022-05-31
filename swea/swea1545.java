2e91836cfce4aa40a52601f4f332d0ac188e7c59
bin <dpqls0625@naver.com>
2022-05-31 20:48:23 +0900

    거꾸로 출력해 보아요

---
+++ swea/swea1545.java
@@ -0,0 +1,14 @@
+package swea;
+import java.util.Scanner;
+public class swea1545 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        for(int test_case = T; test_case >=0 ; test_case--)
+		{
+			System.out.print(test_case+" ");
+        }
+    }
+}
