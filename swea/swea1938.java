5e6176b2c9a4da8b5c713dba5959ae023c23e847
bin <dpqls0625@naver.com>
2022-05-31 19:12:00 +0900

    아주 간단한 계산기

---
+++ swea/swea1938.java
@@ -0,0 +1,14 @@
+package swea;
+import java.util.Scanner;
+public class swea1938 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		int num1= sc.nextInt();
+        int num2=sc.nextInt();
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+    }
+}
