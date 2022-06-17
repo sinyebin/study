e0fdc39660f758f9700f1e6259bf52adcfbf8399
bin <dpqls0625@naver.com>
2022-06-17 17:33:04 +0900

    단어 길이 재기

---
+++ 백준/back2743.java
@@ -0,0 +1,9 @@
+package 백준;
+import java.util.*;
+public class back2743 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        String str=scanner.nextLine();
+        System.out.println(str.length());
+    }
+}
