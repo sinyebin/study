0b8384e0f0b5053b6f909571dabdc4bdd68dc22c
bin <dpqls0625@naver.com>
2022-06-25 11:53:23 +0900

    그대로 출력하기

---
+++ 백준/back11718.java
@@ -0,0 +1,13 @@
+package 백준;
+
+import java.util.Scanner;
+
+public class back11718 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+
+    }
+}
