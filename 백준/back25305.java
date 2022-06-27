885071d74284df10df74e2a26df5ba23e122043f
bin <dpqls0625@naver.com>
2022-06-27 17:51:25 +0900

    커트라인

---
+++ 백준/back25305.java
@@ -0,0 +1,15 @@
+package 백준;
+import java.util.*;
+public class back25305 {
+    public static void main(String args[]){
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int k=scanner.nextInt();
+        Integer arr[]=new Integer[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        Arrays.sort(arr,Collections.reverseOrder());
+        System.out.println(arr[k-1]);
+    }
+}
