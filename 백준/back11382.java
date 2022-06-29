14d894718c22a9e6020cfc6c49be178619b19e96
bin <dpqls0625@naver.com>
2022-06-29 20:22:34 +0900

    꼬마정민

---
+++ 백준/back11382.java
@@ -0,0 +1,12 @@
+package 백준;
+import java.util.*;
+public class back11382 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        long num1=scanner.nextLong();
+        long num2=scanner.nextLong();
+        long num3=scanner.nextLong();
+        
+        System.out.println(num1+num2+num3);
+    }
+}
