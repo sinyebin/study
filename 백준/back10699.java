af6d6873a9daa9398964128571818e5e1a483ee5
bin <dpqls0625@naver.com>
2022-07-07 23:39:46 +0900

    오늘날짜

---
+++ 백준/back10699.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+public class back10699 {  
+	public static void main(String[] args) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date d = new Date();
+		System.out.println(format.format(d));
+	}
+}
+
