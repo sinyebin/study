41c1387f8a03c992f39f8d057856e2943e3fa89b
bin <dpqls0625@naver.com>
2022-07-17 21:20:52 +0900

    사파리월드

---
+++ 백준/back2420.java
@@ -0,0 +1,10 @@
+package 백준;
+import java.util.*;
+public class back2420 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a=scanner.nextInt();
+        long b=scanner.nextInt();
+        System.out.println(Math.abs(a-b));
+    }
+}
