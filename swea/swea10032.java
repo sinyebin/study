ca9bd62c41406a60d7179ec6908fff0b854571ee
bin <dpqls0625@naver.com>
2022-07-31 15:50:23 +0900

    과자 분배

---
+++ swea/swea10032.java
@@ -0,0 +1,20 @@
+package swea;
+import java.util.Scanner;
+public class swea10032 {
+    public static void main(String args[]) throws Exception
+	{
+        Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+        for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int num1=sc.nextInt();
+            int num2=sc.nextInt();
+            if(num1%num2==0){
+             	System.out.println("#"+test_case+" 0");   
+            }else{
+             	   	System.out.println("#"+test_case+" 1");   
+            }
+        }
+    }
+}
