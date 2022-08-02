85194a767d28405115cf6dcdb648d675d0c1d194
bin <dpqls0625@naver.com>
2022-08-03 00:36:13 +0900

    홍익대학교

---
+++ 백준/back16394.java
@@ -0,0 +1,10 @@
+package 백준;
+
+import java.util.*;
+public class back16394 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int num=scanner.nextInt();
+        System.out.println(num-1946);
+    }
+}
