ed48382f93b78e6f2f7784b7a5feb418e5013202
bin <dpqls0625@naver.com>
2022-09-06 23:28:42 +0900

    타임카드 수정

--- 백준/back5575.java
+++ 백준/back5575.java
@@ -6,7 +6,7 @@ public class back5575 {
         for(int i=0;i<3;i++){
             int arr[]=new int[6];
             for(int j=0;j<6;j++){
-                arr[i]=scanner.nextInt();
+                arr[j]=scanner.nextInt();
             }
             int s=arr[5]-arr[2];
             int m=arr[4]-arr[1];
@@ -16,7 +16,7 @@ public class back5575 {
                 m--;
             }
             if(m<0){
-                m=60+s;
+                m=60+m;
                 h--;
             }
             System.out.println(h+" "+m+" "+s);           
