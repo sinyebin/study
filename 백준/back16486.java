3b8c31213875dca126c9215fa1fe44d8e7b80802
bin <dpqls0625@naver.com>
2022-09-09 23:32:08 +0900

    운동장 한 바퀴

---
+++ 백준/back16486.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class back16486 {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        double pi=3.141592;
+        System.out.println(a*2+b*pi*2);
+    }   
+}
