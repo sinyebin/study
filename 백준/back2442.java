96775cc345033cae6b0e75d26e9bb20963dcd2c5
bin <dpqls0625@naver.com>
2022-09-13 23:30:24 +0900

    별 찍기 - 5

---
+++ 백준/back2442.java
@@ -0,0 +1,17 @@
+package 백준;
+import java.util.*;
+public class back2442 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i*2+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
