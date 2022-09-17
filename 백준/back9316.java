547962509abaedf7a26d84f28aebcc71dd4592c4
bin <dpqls0625@naver.com>
2022-09-18 01:22:40 +0900

    Hello Judge

---
+++ 백준/back9316.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class back9316 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        for(int i=0;i<a;i++){
+            System.out.println("Hello World, Judge "+(i+1)+"!");
+        }
+    }
+}
