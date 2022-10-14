2e35f1487853312e77a3742bcbc99512f1b8f8a4
bin <dpqls0625@naver.com>
2022-10-15 00:07:20 +0900

    그대로 출력하기2

---
+++ 백준/back11719.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class back11719 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNextLine()){
+            String str=scanner.nextLine();
+            System.out.println(str);
+        }
+    }
+}
