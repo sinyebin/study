a86144f3ae7111806c719c88eb4871655c1c47e6
bin <dpqls0625@naver.com>
2022-10-27 23:58:14 +0900

    저작권

---
+++ 백준/back2914.java
@@ -0,0 +1,10 @@
+package 백준;
+import java.util.*;
+public class back2914 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        int i=scanner.nextInt()-1;
+        System.out.println(a*i+1);
+    }
+}
