52f9b0edfcbc13545c9f0c66cf0c30bd2621f64f
bin <dpqls0625@naver.com>
2022-11-05 20:43:37 +0900

    대표값2

---
+++ 백준/back2587.java
@@ -0,0 +1,16 @@
+package 백준;
+import java.util.*;
+public class back2587 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[]=new int [5];
+        int sum=0;
+        for(int i=0;i<5;i++){
+            arr[i]=scanner.nextInt();
+            sum+=arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println(sum/5);
+        System.out.println(arr[2]);
+    }
+}
