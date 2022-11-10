23fcfb40e8788e7ffa281166a5b1eea0bdd3b17d
bin <dpqls0625@naver.com>
2022-11-10 21:29:23 +0900

    초콜릿 자르기

---
+++ 백준/back2163.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class back2163 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int m=scanner.nextInt();
+        int c=n*m-1;
+        System.out.println(c);
+    }
+}
