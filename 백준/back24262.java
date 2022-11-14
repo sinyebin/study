bb4a570b70d8dadbf6e2ef61ac2b92417ff9e6e8
bin <dpqls0625@naver.com>
2022-11-14 21:51:50 +0900

    알고리즘 수업 - 알고리즘의 수행 시간 1

---
+++ 백준/back24262.java
@@ -0,0 +1,11 @@
+package 백준;
+
+import java.util.*;
+public class back24262 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
