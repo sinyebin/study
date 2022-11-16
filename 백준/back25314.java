0c612e6d614dc8c1270736fb1d4b01a279db4d71
bin <dpqls0625@naver.com>
2022-11-16 20:54:40 +0900

    코딩은 체육과목입니다.

---
+++ 백준/back25314.java
@@ -0,0 +1,13 @@
+package 백준;
+import java.util.*;
+public class back25314 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        n/=4;
+        for(int i=0;i<n;i++){
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
