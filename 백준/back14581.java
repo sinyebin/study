a87fe28286400b3385b8b9e8241d81f79be449d9
bin <dpqls0625@naver.com>
2022-11-16 20:50:24 +0900

    팬들에게 둘러싸인 홍준

---
+++ 백준/back14581.java
@@ -0,0 +1,11 @@
+package 백준;
+import java.util.*;
+public class back14581 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String id=scanner.nextLine();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::"+id+"::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
