c32f8aad5cfa0ba1c21306202b19300e3f9540b7
bin <dpqls0625@naver.com>
2022-11-30 20:48:06 +0900

    타일 채우기4

---
+++ 백준/back15700.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class back15700 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n=scanner.nextInt();
+        long m=scanner.nextInt();
+        System.out.println((n*m)/2);
+    }
+}
