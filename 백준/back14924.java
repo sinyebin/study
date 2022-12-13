27d562bbacddd81a8ba126b5eb765c4309abc932
bin <dpqls0625@naver.com>
2022-12-13 22:33:36 +0900

    폰 노이만과 파리

---
+++ 백준/back14924.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class back14924{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int s = scanner.nextInt();
+        int t = scanner.nextInt();
+        int d = scanner.nextInt();
+        System.out.println(d/(2*s)*t);
+    }
+}
