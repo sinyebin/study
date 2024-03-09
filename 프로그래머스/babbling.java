f9baed4e66baeb0bfa963ca01dc5220bdd4b7322
bin <dpqls0625@naver.com>
2024-03-09 21:49:14 +0900

    옹알이(2)

---
+++ 프로그래머스/babbling.java
@@ -0,0 +1,27 @@
+package 프로그래머스;
+
+public class babbling {
+    public static void main(String[] args) {
+        String []babbling = {"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        solution(babbling);
+    }
+
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0;i<babbling.length;i++){
+            if(babbling[i].contains("ayaaya")||babbling[i].contains("yeye")||babbling[i].contains("woowoo")||babbling[i].contains("mama")){
+                continue;
+            }
+            babbling[i]=babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+
+            if(babbling[i].length()==0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
